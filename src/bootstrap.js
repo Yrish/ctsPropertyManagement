00fb8b7698a6e50f993d2dd583e67dec44e2da95
Braysen Goodwin <braysenlilgoodwin@gmail.com>
2018-07-21 15:23:16 -0600

    authentication

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import App from './components/app';
 import reducers from './reducers';
 import Signin from './components/auth/Signin'
 import Signup from './components/auth/Signup'
 import HeaderWrapper from './components/HeaderWrapper'
 import HeaderBar from './components/headerBar'
+import history from './history'
+import Dashboard from './components/Dashboard'
+import requireAuth from './components/requireAuth'
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f=>f)(createStore)));
 
@@ -23,13 +26,15 @@ function main() {
         <div id="application">
           <HeaderWrapper></HeaderWrapper>
           <HeaderBar/>
-          <BrowserRouter>
+          <Router history={history}>
             <Switch>
               <Route path='/' exact component={Signin}/>
               <Route path='/signup' component={Signup}/>
               <Route path='/signin' component={Signin}/>
+
+              <Route path='/dashboard' component={requireAuth(Dashboard)}/>
             </Switch>
-          </BrowserRouter>
+          </Router>
         </div>
       </Provider>
     , document.querySelector('.app-wrapper'));
---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+export default class Dashboard extends Component<Props> {
+  render() {
+    return (
+      <div>dashboard Component</div>
+    )
+  }
+}
